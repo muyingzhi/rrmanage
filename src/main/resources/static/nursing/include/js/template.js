45a449dddc10193f44c8ae1eb69bf948a7af91d4
muyz.york <yingzhi.mu@live.cn>
2021-02-24 22:40:12 +0800

    护士数据列表

--- src/main/resources/static/nursing/include/js/template.js
+++ src/main/resources/static/nursing/include/js/template.js
@@ -24,7 +24,7 @@ var initdata = new Vue({
 	},
 	mounted: function() {
 		var that = this;
-		axios.post(serverurl + 'api/user/list').then(function(res) {
+		axios.post(serverurl + 'api/user/nurse/list').then(function(res) {
 			that.userList = res.data.data;
 		})
 		this.loadtable();
